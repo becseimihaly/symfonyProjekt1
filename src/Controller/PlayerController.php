4242931b531a17c300ddf50dfee9b02861859719
becseimihaly <bemihaly@gmail.com>
2022-06-02 15:20:41 +0200

    after video 14

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -3,22 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+//use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class PlayerController extends AbstractController
 {
     /**
      * @Route("/api/player/{id<\d+>}", name="api/player", methods={"GET"})
      */
-    public function getPlayer(int $id): Response
+    public function getPlayer(int $id, LoggerInterface $logger): Response
     {
         $player =[
             'id'=>$id,
             'name'=> 'Finn \'Karrigan\' Andersen',
             'link'=> 'https://www.hltv.org/player/429/karrigan'
         ];
+
+        $logger->info('visszaadva {player}', [
+            'song'=> $id
+        ]);
 
         //return new JsonResponse($player);
         return $this->json($player);
