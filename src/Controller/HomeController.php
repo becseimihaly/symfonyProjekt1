beb4d369b8fab4609b10d1645d3d0fb34e81dc0b
becseimihaly <bemihaly@gmail.com>
2022-06-02 09:25:26 +0200

    after video 8

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,7 +21,30 @@ class HomeController extends AbstractController
      */
     public function twig(): Response
     {
-        return $this->render('home/twig.html.twig', ['title'=>'Twig Page']);
+
+        $americanLeague = [
+            'Rays', 'Yankees', 'Blue Jays', 'Orioles', 'Red Sox', 'Royals', 'Twins',
+            'Tigers', 'Indians', 'White Sox', 'Astros', 'Rangers', 'Angels',
+            'Mariners', 'Athletics'
+        ];
+
+        $classicCars = [
+            ['year'=>1953, 'brand'=>'Chevy', 'type'=>'Corvette'],
+            ['year'=>1966, 'brand'=>'Ford', 'type'=>'Mustang'],
+            ['year'=>1967, 'brand'=>'Pontiac', 'type'=>'GTO'],
+            ['year'=>1969, 'brand'=>'Dodge', 'type'=>'Charger'],
+            ['year'=>1962, 'brand'=>'Chevy', 'type'=>'Impala'],
+            ['year'=>1968, 'brand'=>'Chevy', 'type'=>'Camaro'],
+            ['year'=>1962, 'brand'=>'Ford', 'type'=>'Thunderbird'],
+            ['year'=>1957, 'brand'=>'Chevy', 'type'=>'Bel Air'],
+            ['year'=>1966, 'brand'=>'Lincoln', 'type'=>'Continental']
+        ];
+
+        return $this->render('home/twig.html.twig', [
+            'title'=>'Twig Page',
+            'americanLeague'=>$americanLeague,
+            'classicCars'=>$classicCars
+        ]);
     }
 
     /**
